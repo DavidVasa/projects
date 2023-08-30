6ddb6ccff5e1a41553afbce5febd72cbf7333454
Pavel Vršecký <pavlicekvr@gmail.com>
2023-08-30 23:09:32 +0200

    first program

---
+++ rock_scissors_paper/Main.java
@@ -0,0 +1,13 @@
+package rock_scissors_paper;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String choice = userInput.nextLine();
+        String[] options = { "rock", "paper", "scissors" };
+
+    }
+}
