3098bdf04bcb6f53620c57fb3a72d7bc6ebddb77
Pavel Vršecký <pavlicekvr@gmail.com>
2023-08-31 00:53:08 +0200

    Ultimate version

--- rock_scissors_paper/Main.java
+++ rock_scissors_paper/Main.java
@@ -1,6 +1,9 @@
 package rock_scissors_paper;
 
 import java.util.Scanner;
+
+import javax.print.DocFlavor.READER;
+
 import java.util.Arrays;
 import java.util.Random;
 
@@ -8,18 +11,51 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner userInput = new Scanner(System.in);
-        String[] options = { "rock", "paper", "scissors" };
+        String RESET = "\u001B[0m";
+        String RED = "\u001B[31m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        String WHITE_BG = "\u001B[47m";
+        String BLUE = "\u001B[34m";
+        String CURRENT_COLOR = "";
+        String[] options = { "rock", "paper", "scissors", "exit" };
         String userChoice = "";
+        int score = 0;
+        System.out.println(GREEN + WHITE_BG + "Welcome to Rock Scissors Paper game!\n" + RESET);
 
-        System.out.println("Choose one of the options: rock, paper, scissors");
-        while (!Arrays.asList(options).contains(userChoice = userInput.nextLine())) {
-            System.out.println("You misspelled the choice! Try again bastard!");
+        while (true) {
+            System.out.println("Choose one of the options: rock, paper, scissors");
+            if (score > 0)
+                CURRENT_COLOR = GREEN;
+            else if (score < 0)
+                CURRENT_COLOR = RED;
+            else
+                CURRENT_COLOR = RESET;
+            System.out.println(CURRENT_COLOR + "Your Score: " + score + RESET);
+            System.out.println("Write 'exit' if you want to quit the game. \n");
+            System.out.print(BLUE + "Your choice: " + RESET);
+            while (!Arrays.asList(options).contains(userChoice = userInput.nextLine())) {
+                System.out.println(RED + "You misspelled the choice! Try again bastard!\n" + RESET);
+                System.out.print(BLUE + "Your choice: " + RESET);
+            }
+            int randomNumber = random.nextInt(3);
+            String botChoice = options[randomNumber];
+
+            if (userChoice.equals("exit"))
+                break;
+            else if (botChoice.equals(userChoice))
+                System.out.println(YELLOW + "Draw \n" + RESET);
+
+            else if (userChoice.equals("rock") && botChoice.equals("scissors")
+                    || userChoice.equals("paper") && botChoice.equals("rock")
+                    || userChoice.equals("scissors") && botChoice.equals("paper")) {
+                System.out.println(GREEN + "You won! The bot chose " + botChoice + "\n" + RESET);
+                score++;
+            } else {
+                System.out.println(RED + "You lost. The bot chose " + botChoice + "\n" + RESET);
+                score--;
+            }
+
         }
-        int randomNumber = random.nextInt(1);
-        String botChoice = options[randomNumber];
-
-        if (botChoice.equals(userChoice))
-            System.out.println("Draw");
-
     }
 }
