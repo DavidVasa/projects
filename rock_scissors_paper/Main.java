f0693b68ffa7cf1ff3a85639ef4279a8858cc37c
Pavel Vršecký <pavlicekvr@gmail.com>
2023-08-30 23:27:57 +0200

    new changes

--- rock_scissors_paper/Main.java
+++ rock_scissors_paper/Main.java
@@ -1,13 +1,21 @@
 package rock_scissors_paper;
 
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
-        String choice = userInput.nextLine();
         String[] options = { "rock", "paper", "scissors" };
-
+        Scanner userInput = new Scanner(System.in);/*
+                                                    * while (!Arrays.asList(options).contains(userInput.nextLine())) {
+                                                    * userInput.nextLine();
+                                                    * }
+                                                    */
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            int botChoice = random.nextInt(3);
+            System.out.println(botChoice);
+        }
     }
 }
