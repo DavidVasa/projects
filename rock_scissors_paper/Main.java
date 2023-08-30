ba06e8b3784d355ec26a9c22de2431ceed83071d
Pavel Vršecký <pavlicekvr@gmail.com>
2023-08-30 23:47:19 +0200

    fix bug with loading

--- rock_scissors_paper/Main.java
+++ rock_scissors_paper/Main.java
@@ -7,18 +7,16 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
+        Scanner userInput = new Scanner(System.in);
         String[] options = { "rock", "paper", "scissors" };
         String userChoice = "";
 
-        Scanner userInput = new Scanner(System.in);
-        while (!Arrays.asList(options).contains(userChoice=userInput.nextLine())) {
-            userInput.nextLine();
-        userChoice = userInput;
+        System.out.println("Choose one of the options: rock, paper, scissors");
+        while (!Arrays.asList(options).contains(userChoice = userInput.nextLine())) {
+            System.out.println("You misspelled the choice! Try again bastard!");
         }
         int randomNumber = random.nextInt(3);
         String botChoice = options[randomNumber];
-
-        if ()
 
     }
 }
