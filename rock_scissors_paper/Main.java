4d668705c72a99eeba6d49eff581e67923f3e53b
David Váša <rengaros@email.cz>
2023-08-30 23:38:41 +0200

    working version 3

--- rock_scissors_paper/Main.java
+++ rock_scissors_paper/Main.java
@@ -8,13 +8,17 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         String[] options = { "rock", "paper", "scissors" };
-        Scanner userInput = new Scanner(System.in);
-        while (!Arrays.asList(options).contains(userInput.nextLine())) {
-            userInput.nextLine();
-        }
+        String userChoice = "";
 
+        Scanner userInput = new Scanner(System.in);
+        while (!Arrays.asList(options).contains(userChoice=userInput.nextLine())) {
+            userInput.nextLine();
+        userChoice = userInput;
+        }
         int randomNumber = random.nextInt(3);
         String botChoice = options[randomNumber];
+
+        if ()
 
     }
 }
