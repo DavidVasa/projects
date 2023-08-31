c48d1190de306dec7250b74f64c09496ef76083d
Pavel Vršecký <pavlicekvr@gmail.com>
2023-09-01 01:39:57 +0200

    random obstacles added

--- dungeon_game/ObstacleGeneration.java
+++ dungeon_game/ObstacleGeneration.java
@@ -1,26 +1,22 @@
 package dungeon_game;
-import java.util.Random;
 
+import java.util.Random;
 
 public class ObstacleGeneration {
     public MapGeneration map;
-    Random randomX = new Random(map.sizeX-1);
-    Random randomY = new Random(map.sizeY-1);
-    String[][] obstacleMap = new String[map.sizeX][map.sizeY]; 
+    Random randomX = new Random();
+    Random randomY = new Random();
 
     public ObstacleGeneration(MapGeneration map) {
         this.map = map;
     }
 
-    public String[][] createObstacle(int obstacleCount) {
-
-            for (int i = 0; i < obstacleCount; i++)
-            {
-
-                obstacleMap[randomX+1][randomY+1] = "*"; 
-            }
-
-        return obstacleMap;
+    public String[][] createObstacle(int obstacleCount, String[][] generatedMap) {
+        for (int i = 0; i < obstacleCount; i++) {
+            generatedMap[randomX.nextInt(generatedMap.length - 2) + 1][randomY.nextInt(generatedMap[0].length - 2)
+                    + 1] = "*";
+        }
+        return generatedMap;
     }
 
 }
